8c88dba77c47688493bc0c5be59e48b94f7f3a93
Felipecpo1 <felipeppt1@gmail.com>
2024-03-16 00:54:59 -0300

    comecando botao copiar

--- index.js
+++ index.js
@@ -3,21 +3,26 @@
 function encriptar () {
     let input = document.querySelector('.textoDigitado');
     let texto = input.value;
-    var codificado = texto.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat')
-    console.log(codificado)
-    
+    let codificado = texto.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
+    exibirTexto(codificado);
 }
 
 function descriptar () {
     let input = document.querySelector('.textoDigitado');
     let texto = input.value;
-    var decodificado = texto.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u')
-    console.log(decodificado)
-
+    let decodificado = texto.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');
+    exibirTexto(decodificado);
 }
 
+function exibirTexto (textoParametro) {
+    let secaoTexto = document.querySelector('.exibirTexto')
+    secaoTexto.innerHTML = textoParametro;
+    return textoParametro;
+}
 
-//var palavra = "carrao cavalo";
-//var novaPalavra = palavra.replace(/a/g, "b");
-//
-//console.log(novaPalavra);
+function copiarDecodificado () {
+    let textoDecod = document.querySelector('.exibirTexto');
+    let copiar = textoDecod.textContent;
+    let input = document.querySelector('.textoDigitado');
+    input.value = copiar
+}
