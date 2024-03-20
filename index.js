6a4077508978c03f2c025c6aca85318fd837307c
Felipecpo1 <felipeppt1@gmail.com>
2024-03-20 00:21:30 -0300

    inserindo icones

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ function copiarTexto() {
     let textoParaCopiar = document.querySelector('.exibirTexto').innerHTML;;
     let botao = document.querySelector('.botaoCopiar');
     navigator.clipboard.writeText(textoParaCopiar);
-    alert('Texto copiado: ' + textoParaCopiar);
-    
+    botao.innerHTML = 'Copiado <i class="fa-solid fa-check"></i>';
+}
+
+const statusBotaoCopiar = () => {
+    let botao = document.querySelector('.botaoCopiar');
+    botao.innerHTML = '<i class="fa-solid fa-copy"></i> Copiar'
 }
 
 const controleDeExibicao = () => {
@@ -35,7 +39,8 @@ function encriptar () {
     let texto = input.value;
     let codificado = texto.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
     exibirTexto(codificado);
-    controleDeExibicao()
+    controleDeExibicao();
+    statusBotaoCopiar();
 }
 
 function descriptar () {
@@ -44,5 +49,6 @@ function descriptar () {
     let decodificado = texto.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');
     exibirTexto(decodificado);
     controleDeExibicao();
+    statusBotaoCopiar();
 }
 
